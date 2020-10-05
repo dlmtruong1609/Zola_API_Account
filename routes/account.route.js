0c86af8d43dcb1281b8d55e524fc67c7632ba076
dlmtruong1609 <hinatosss111@gmail.com>
2020-10-05 15:13:07 +0700

    allow host http://localhost:19002

--- routes/account.route.js
+++ routes/account.route.js
@@ -16,4 +16,15 @@ router.get('/api/v0/accounts/active/send', accountService.sendSMSActiveAgain)
 
 router.post('/api/v0/accounts/code/verify', accountService.verifyCode)
 
+router.post('/test', (req, res) => {
+  const accountSid = 'AC5ef1f1d17d9b491ff1fd08d24a902deb'
+  const authToken = '30b750b69fef4aea8badd765fa935ea0'
+  const client = require('twilio')(accountSid, authToken)
+
+  client.verify.services('VA58fae4e018d2545d28967df15a8777c3')
+    .verifications
+    .create({ to: 'dlmtruong1609@gmail.com', channel: 'email' })
+    .then(verification => console.log(verification.sid))
+})
+
 module.exports = router
