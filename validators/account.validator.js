59861e5d14133570ed8f7211b70dd8bd85b8c42a
dlmtruong1609 <hinatosss111@gmail.com>
2020-10-21 21:18:27 +0700

    fix api sign up

--- validators/account.validator.js
+++ validators/account.validator.js
@@ -1,49 +1,42 @@
-const { check, query } = require('express-validator')
+const { check, query, header } = require('express-validator')
 const bcrypt = require('bcryptjs')
 const db = require('../models')
 const Account = db.account
 const CONSTANT = require('../utils/account.constants')
+
+require('dotenv').config()
+const jwtHelper = require('../helpers/jwt.helper')
+const accessTokenSecret = process.env.ACCESS_TOKEN_SECRET
+
 const validateSignUp = () => {
   return [
-    check('phone').custom((value, { req }) => {
-      const phone = req.body.phone
-      const email = req.body.email
-      if (email || phone) {
-        return true
-      }
-      throw new Error('Please enter phone or email')
-    }),
     check('name', CONSTANT.NAME_IS_REQUIRED).not().isEmpty(), // validate để trống trường email sử dụng hàm notEmpty()
     check('name', CONSTANT.NAME_SIZE).isLength({ min: 6, max: 32 }),
-    check('phone', CONSTANT.PHONE_IS_REQUIRED).optional({ checkFalsy: true }).not().isEmpty(),
-    check('phone').custom((value, { req }) => {
-      const phone = req.body.phone
-      const email = req.body.email
-      if (email || phone) {
-        return true
+    header('x-access-token').custom(async (value, { req }) => {
+      const decoded = await jwtHelper.verifyToken(
+        req.headers['x-access-token'],
+        accessTokenSecret
+      )
+      const accountDecode = decoded.data
+      const phone = accountDecode.phone
+      const email = accountDecode.email
+      if (phone) {
+        return Account.findOne({
+          where: { phone: phone }
+        }).then((account) => {
+          if (account) {
+            return Promise.reject(CONSTANT.PHONE_AVAILABLE)
+          }
+        })
+      } else {
+        return Account.findOne({
+          where: { email: email }
+        }).then((account) => {
+          if (account) {
+            return Promise.reject(CONSTANT.EMAIL_AVAILABLE)
+          }
+        })
       }
-      throw new Error('Please enter phone or email')
-    }),
-    check('phone', CONSTANT.IS_PHONE).optional({ checkFalsy: true }).matches(/((09|03|07|08|05)+([0-9]{8})\b)/),
-    check('phone').optional({ checkFalsy: true }).custom((value, { req }) => {
-      return Account.findOne({
-        where: { phone: value }
-      }).then((account) => {
-        if (account) {
-          return Promise.reject(CONSTANT.PHONE_AVAILABLE)
-        }
-      })
-    }),
-    check('email', CONSTANT.EMAIL_IS_REQUIRED).optional({ checkFalsy: true }).not().isEmpty(),
-    check('email', CONSTANT.IS_EMAIL).optional({ checkFalsy: true }).isEmail(),
-    check('email').optional({ checkFalsy: true }).custom((value, { req }) => {
-      return Account.findOne({
-        where: { email: value }
-      }).then((account) => {
-        if (account) {
-          return Promise.reject(CONSTANT.EMAIL_AVAILABLE)
-        }
-      })
     }),
     check('password', CONSTANT.PASSWORD_IS_REQUIRED).not().isEmpty(),
     check('password', CONSTANT.PASSWORD_SIZE).isLength({ min: 6, max: 32 }),
