a007c44a4448f75230c2df585a226d0fb804d77e
truongdlm <truongdlm@fpt.com.vn>
2020-11-15 16:10:04 +0700

    change port postgres 5444

--- db/index.js
+++ db/index.js
@@ -4,6 +4,7 @@ module.exports = {
   USER: process.env.DB_USER,
   PASSWORD: process.env.DB_PASSWORD,
   DB: process.env.DB_NAME,
+  PORT: process.env.DB_PORT,
   dialect: 'postgres',
   pool: {
     max: 5,
